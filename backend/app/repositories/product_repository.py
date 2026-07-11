8645bb7b7a8210a830ffef6faa4b2b78f4e99140
noonaxl <woman.com.ua@gmail.com>
2026-07-11 17:53:12 +0300

    Fix problems

--- backend/app/repositories/product_repository.py
+++ backend/app/repositories/product_repository.py
@@ -1,28 +1,36 @@
 from sqlalchemy.orm import Session, joinedload
 from typing import List, Optional
+
 from ..models.product import Product
 from ..schemas.product import ProductCreate
 
 
-class ProductRepository():
+class ProductRepository:
     def __init__(self, db: Session):
         self.db = db
 
     def get_all(self) -> List[Product]:
-        return self.db.query(Product).option(joinedload(Product.category)).all()
+        return (
+            self.db.query(Product)
+            .options(joinedload(Product.category))
+            .all()
+        )
 
     def get_by_id(self, product_id: int) -> Optional[Product]:
-        return (self.db.query(Product)
-                .options(joinedload(Product.category))
-                .filter(Product.id == product_id)
-                .first()
-                )
+        return (
+            self.db.query(Product)
+            .options(joinedload(Product.category))
+            .filter(Product.id == product_id)
+            .first()
+        )
 
-    def get_by_category_id(self, category_id: int) -> List[Product]:
-        return (self.db.query(Product)
-                .options(joinedload(Product.category))
-                .filter(Product.category_id == category_id)
-                .all())
+    def get_by_category(self, category_id: int) -> List[Product]:
+        return (
+            self.db.query(Product)
+            .options(joinedload(Product.category))
+            .filter(Product.category_id == category_id)
+            .all()
+        )
 
     def create(self, product_data: ProductCreate) -> Product:
         db_product = Product(**product_data.model_dump())
@@ -32,7 +40,9 @@ class ProductRepository():
         return db_product
 
     def get_multiple_by_ids(self, product_ids: List[int]) -> List[Product]:
-        return (self.db.query(Product)
-                .options(joinedload(Product.category))
-                .filter(Product.id.in_(product_ids))
-                .all())
+        return (
+            self.db.query(Product)
+            .options(joinedload(Product.category))
+            .filter(Product.id.in_(product_ids))
+            .all()
+        )
--- backend/app/services/product_services.py
+++ backend/app/services/product_services.py
@@ -1,9 +1,16 @@
-from sqlalchemy.orm import Session
 from typing import List
-from ..repositories.product_repository  import ProductRepository
-from ..repositories.category_repository  import CategoryRepository
-from ..schemas.product import ProductResponse, ProductListResponse, ProductCreate
+
 from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+
+from ..repositories.category_repository import CategoryRepository
+from ..repositories.product_repository import ProductRepository
+from ..schemas.product import (
+    ProductCreate,
+    ProductListResponse,
+    ProductResponse,
+)
+
 
 class ProductService:
     def __init__(self, db: Session):
@@ -11,30 +18,69 @@ class ProductService:
         self.category_repository = CategoryRepository(db)
 
     def get_all_products(self) -> ProductListResponse:
-        products = self.product_repository.all()
-        products_response = [ProductListResponse.model_response(prod) for prod in products]
-        return ProductListResponse(products=products_response, total=len(products_response))
+        products = self.product_repository.get_all()
+
+        products_response = [
+            ProductResponse.model_validate(product)
+            for product in products
+        ]
+
+        return ProductListResponse(
+            products=products_response,
+            total=len(products_response),
+        )
 
     def get_product_by_id(self, product_id: int) -> ProductResponse:
-        product = self.product_repository.get(product_id)
+        product = self.product_repository.get_by_id(product_id)
+
         if not product:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"Product with id {product_id} not found")
-        return ProductResponse.model_response(product)
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Product with id {product_id} not found",
+            )
 
-    def get_products_by_category(self, category_id: int) -> List[ProductResponse]:
-        category = self.category_repository.get(category_id)
+        return ProductResponse.model_validate(product)
+
+    def get_products_by_category(
+        self,
+        category_id: int,
+    ) -> ProductListResponse:
+
+        category = self.category_repository.get_by_id(category_id)
+
         if not category:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"Category with id {category_id} not found")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Category with id {category_id} not found",
+            )
+
         products = self.product_repository.get_by_category(category_id)
-        products_response = [ProductListResponse.model_validate (prod) for prod in products]
-        return ProductListResponse(products=products_response, total=len(products_response))
 
-    def create_product(self, product_data: ProductCreate) -> ProductResponse:
-        category = self.category_repository.get(product_data.category_id)
+        products_response = [
+            ProductResponse.model_validate(product)
+            for product in products
+        ]
+
+        return ProductListResponse(
+            products=products_response,
+            total=len(products_response),
+        )
+
+    def create_product(
+        self,
+        product_data: ProductCreate,
+    ) -> ProductResponse:
+
+        category = self.category_repository.get_by_id(
+            product_data.category_id
+        )
+
         if not category:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"Category with id {product_data.category_id} not found")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Category with id {product_data.category_id} not found",
+            )
+
         product = self.product_repository.create(product_data)
+
         return ProductResponse.model_validate(product)
